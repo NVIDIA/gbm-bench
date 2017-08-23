9caf39cbc72dfe90e547d45f97ca5eb68b860359
Andy Adinets <aadinets@nvidia.com>
2017-08-23 17:02:04 +0200

    Added -benchmarks option, to run only a subset of benchmarks at a time.

--- runme.py
+++ runme.py
@@ -14,17 +14,22 @@ def parseArgs():
                         help="The dataset to be used for benchmarking")
     parser.add_argument("-root", default="/datasets",
                         type=str, help="The root datasets folder")
+    parser.add_argument("-benchmarks", default="all", type=str,
+                         help="The comma-separated list of benchmarks to run; 'all' run all of them")
     args = parser.parse_args()
     return args
 
 # benchmarks a single dataset
-def benchmark(dbFolder, module):
+def benchmark(dbFolder, module, benchmarks):
     warnings.filterwarnings('ignore')
     data = module.prepare(dbFolder)
     funcs = module.benchmarks
     results = {}
-    for (name, class_params) in funcs.items():
-        cls, params = class_params
+    # 'all' runs all benchmarks
+    if benchmarks[0] == 'all':
+        benchmarks = funcs.keys()
+    for name in benchmarks:
+        cls, params = funcs[name]
         print("Running '%s' ..." % name)
         results[name] = cls(data, params).run()
     
@@ -34,9 +39,10 @@ def main():
     args = parseArgs()
     utils.print_sys_info()
     folder = os.path.join(args.root, args.dataset)
+    benchmarks = args.benchmarks.split(',')
     # TODO: this is a HACK to support dynamic loading of modules at runtime!
     module = __import__(args.dataset)
-    results = benchmark(folder, module)
+    results = benchmark(folder, module, benchmarks)
     print(json.dumps(results, indent=2, sort_keys=True))
     return
 
