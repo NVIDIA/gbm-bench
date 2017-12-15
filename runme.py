9df5c7d029fa9cc68099503e9f6b3efc3d8e295f
Shankara Rao Thejaswi Nanditale <snanditale@nvidia.com>
2017-12-15 10:27:10 -0800

    added options to generate custom output json file and overriding max-depth and ntrees

--- runme.py
+++ runme.py
@@ -22,8 +22,44 @@ def parseArgs():
     parser.add_argument("-ncpus", default=0, type=int,
                         help=("#CPUs to use for the benchmarks; "
                               "0 means multiprocessing.cpu_count()"))
+    parser.add_argument("-output", default=None, type=str,
+                        help="Output json file with runtime/accuracy stats")
+    parser.add_argument("-maxdepth", default=None, type=int,
+                        help=("Max-depth of trees. Default is as specified in "
+                              "the respective dataset configuration"))
+    parser.add_argument("-ntrees", default=None, type=int,
+                        help=("Number of trees. Default is as specified in "
+                              "the respective dataset configuration"))
     args = parser.parse_args()
+    # default value for output json file
+    if not args.output:
+        args.output = "%s.json" % args.dataset
     return args
+
+# add extra parameters for benchmarks (if present)
+def addExtraParams(params, extraParams, bName):
+    # multi gpu case
+    if "n_gpus" in extraParams:
+        if "xgb" in bName:
+            params[extra_key] = extra_value
+        else:
+            print("'n_gpus' currently only applies to 'xgboost'")
+    # if need to customize tree depth
+    if "maxdepth" in extraParams:
+        if "xgb" in bName:
+            params["max_depth"] = extraParams["maxdepth"]
+            params["max_leaves"] = 2**extraParams["maxdepth"]
+        elif "lgbm" in bName:
+            params["num_leaves"] = 2**extraParams["maxdepth"]
+        elif "cat" in bName:
+            params["depth"] = extraParams["maxdepth"]
+    # if need to customize number of boosters
+    if "ntrees" in extraParams:
+        if "xgb" in bName or "lgbm" in bName:
+            params["num_round"] = extraParams["ntrees"]
+        elif "cat" in bName:
+            params["iterations"] = extraParams["ntrees"]
+    return
 
 # benchmarks a single dataset
 def benchmark(dbFolder, module, benchmarks, extra_params):
@@ -39,14 +75,7 @@ def benchmark(dbFolder, module, benchmarks, extra_params):
         if not enabled:
             print("Skipping '%s'... " % name)
             continue
-        # add extra parameters for benchmarks (if present)
-        for (extra_key, extra_value) in extra_params.items():
-            # only set n_gpus for xgboost,
-            # as LightGBM/CatBoost do not support it
-            # this currently only affects xgb-gpu-hist benchmarks
-            if extra_key == "n_gpus" and "xgb" not in name:
-                continue
-            params[extra_key] = extra_value
+        addExtraParams(params, extra_params, name)
         print("Running '%s' ..." % name)
         runner = cls(data, params)
         (train_time, test_time) = runner.run()
@@ -67,12 +96,17 @@ def main():
     # TODO: this is a HACK to support dynamic loading of modules at runtime!
     module = __import__(args.dataset)
     extra_params = {"n_gpus": args.ngpus}
+    if args.maxdepth is not None:
+        extra_params["maxdepth"] = args.maxdepth
+    if args.ntrees is not None:
+        extra_params["ntrees"] = args.ntrees
     results = benchmark(folder, module, benchmarks, extra_params)
     output = json.dumps(results, indent=2, sort_keys=True)
     print(output)
-    fp = open("%s.json" % args.dataset, "w")
+    fp = open(args.output, "w")
     fp.write(output + "\n")
     fp.close()
+    print("Results written to file '%s'" % args.output)
 
 if __name__ == "__main__":
     main()
