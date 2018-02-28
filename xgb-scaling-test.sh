fcc35d554b049aae2d759dd067942b692756330c
Shankara Rao Thejaswi Nanditale <snanditale@nvidia.com>
2018-02-27 22:23:15 -0800

    added OMP flags which are needed on a DGX box, else the perf numbers are not trustworthy

--- xgb-scaling-test.sh
+++ xgb-scaling-test.sh
@@ -17,17 +17,58 @@
 # to the stdout, as a function of number of GPUs and also a function of whether
 # cpu or gpu based prediction was used
 #
-dataset=${1:-airline_ext}
+
+function cudaVisibleDevices() {
+    local ngpus=$1
+    local isvolta=$2
+    # in case of DGX1v, there are gpu pairs with double connections between them
+    # and that's because every GPU has 6 nvlink connections as opposed to 4 in
+    # DGX1p
+    if [ "$dgx1v" = "1" ]; then
+        case $ngpus in
+            1)
+                echo "0";;
+            2)
+                echo "0,3";;
+            4)
+                echo "0,1,2,3";;
+            8)
+                echo "0,3,2,1,5,6,7,4";;
+        esac
+    else
+        case $ngpus in
+            1)
+                echo "0";;
+            2)
+                echo "0,2";;
+            4)
+                echo "0,1,2,3";;
+            8)
+                echo "0,1,2,3,4,5,6,7";;
+        esac
+    fi
+}
+
+dgx1v=${1:-0}
+ncpus=40
+dataset=airline_ext
 export PYTHONPATH=`pwd`:`pwd`/../python-package
 touch catboost.py lightgbm.py
 for ngpus in 1 2 4 8; do
+    devs=`cudaVisibleDevices $ngpus $dgx1v`
     for pred in cpu gpu; do
-        ../../gbm-bench/runme.py -root ../../gbm-datasets/ \
-                                 -dataset $dataset \
-                                 -benchmark xgb-gpu-hist \
-                                 -ngpus $ngpus \
-                                 -extra "{'predictor':'${pred}_predictor'}" \
-                                 -output airline_ext_${pred}_${ngpus}.json
+        env CUDA_VISIBLE_DEVICES=$devs \
+            OMP_PROC_BIND=TRUE \
+            OMP_NUM_THREADS=$ncpus \
+            OMP_PLACES="{0}:$ncpus" \
+            OMP_DISPLAY_ENV=TRUE \
+            ../../gbm-bench/runme.py -root ../../gbm-datasets/ \
+                -dataset $dataset \
+                -benchmark xgb-gpu-hist \
+                -ngpus $ngpus \
+                -ncpus $ncpus \
+                -extra "{'predictor':'${pred}_predictor'}" \
+                -output ${dataset}_${pred}_${ngpus}.json
     done
 done
 ../../gbm-bench/json2csv.py *.json
